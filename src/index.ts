d0b5064781da177666ebd3f9af8de0d513165bb8
Sajankumar Vijayan <sajankumarvijayan@Sajankumars-Air.home>
2021-01-16 18:13:57 +0100

    formatted

--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,53 @@
-import EditorJs from '@editorjs/editorjs';
+import EditorJs from "@editorjs/editorjs";
 import Header from "@editorjs/header";
-import Image from '@editorjs/image';
-import Paragraph from '@editorjs/paragraph';
-import './css/main.scss';
+import Image from "@editorjs/image";
+import Paragraph from "@editorjs/paragraph";
+import "./css/main.scss";
 
-
-function convertBase64(file: File):Promise<string> {
-	return new Promise((resolve, reject) => {
-		const fr = new FileReader();
+function convertBase64(file: File): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const fr = new FileReader();
     fr.onload = function () {
-			resolve(fr.result as string);
-		};
-		fr.onerror = function () {
-			reject(new Error('failed to convert'))
-		}
+      resolve(fr.result as string);
+    };
+    fr.onerror = function () {
+      reject(new Error("failed to convert"));
+    };
     fr.readAsDataURL(file);
-	})
+  });
 }
 
 const editorjs = new EditorJs({
   holder: "editorjs",
-	placeholder: "Type here...",
-	tools: {
-		header: Header,
-		paragraph: {
-			class: Paragraph
-		},
-		image: {
-			class: Image,
-			config: {
-				uploader: {
-					async uploadByFile(file: File) {
-						return {
+  placeholder: "Type here...",
+  tools: {
+    header: Header,
+    paragraph: {
+      class: Paragraph,
+    },
+    image: {
+      class: Image,
+      config: {
+        uploader: {
+          async uploadByFile(file: File) {
+            return {
               success: 1,
               file: {
-                url: await convertBase64(file)
+                url: await convertBase64(file),
               },
             };
-					}
-				}
-			}
-		}
-	}
+          },
+        },
+      },
+    },
+  },
 });
 
-
-document.querySelector('#saveBtn').addEventListener('click', async () => {
-	try {
-		const jsonData = await editorjs.save();
-		console.log(jsonData);
-	}catch(e) {
-		console.error(e)
-	}
-})
+document.querySelector("#saveBtn").addEventListener("click", async () => {
+  try {
+    const jsonData = await editorjs.save();
+    console.log(jsonData);
+  } catch (e) {
+    console.error(e);
+  }
+});
